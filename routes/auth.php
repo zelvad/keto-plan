529a2a6f07b86d0d5075884eb0af7ad3813d09a7
zelvad <vladislav.zelenyk@scalemeup.com>
2022-11-08 19:58:56 +0200

    fix

--- routes/auth.php
+++ routes/auth.php
@@ -19,7 +19,7 @@ Route::middleware('guest')->group(function () {
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::view('ketoplan-subscribe', 'client.auth.ketoplan-subscribe');
+    Route::view('fitoplan-subscribe', 'client.auth.ketoplan-subscribe');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
